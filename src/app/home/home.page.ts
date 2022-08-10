f5be1486ffbdc2d43e39e07f18cb16194034af8e
alkcode <alek_1604@comunidad.unam.mx>
2022-08-10 11:33:17 -0500

    Pantalla de muestra pendiente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
 
+interface Datos{
+  titulo:string;
+  img: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  datos:Datos[]= [
+    {
+      titulo:"Convertidor catalitico universal OBD ll",
+      img:"../../assets/img/Forma 1.png",
+    },
+    {
+      titulo:"Convertidor catalitico universal OBD ll",
+      img:"../../assets/img/Forma 2.png",
+    },
+    {
+      titulo:"Convertidor catalitico universal OBD ll",
+      img:"../../assets/img/Forma 3.png",
+    },
+    {
+      titulo:"Cartucho OBD ll",
+      img:"../../assets/img/Forma 4.png",
+    },
+    {
+      titulo:"Cartucho OBD ll",
+      img:"../../assets/img/Forma 5.png",
+    },
+    {
+      titulo:"Cartucho OBD ll",
+      img:"../../assets/img/Forma 6.png",
+    },
+  ];
 
   constructor() {}
 
